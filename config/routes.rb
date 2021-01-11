6180b677481c121e40a2a210a8b18f7fdf1ba34d
EC2 Default User <ec2-user@ip-172-31-12-154.ec2.internal>
2021-01-11 14:33:20 +0000

    user/show途中

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#top'
   get 'homes/about'
+  resources :users, only: [:show]
   resources :movies, only: [:index, :show, :create, :edit, :update, :destroy]
 end
